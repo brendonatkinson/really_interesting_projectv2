7a745f6d8b40db42215c29f289b52d016f809f9a
brendon <brendon.atkinson@gmail.com>
2017-03-20 19:48:14 +1300

    First version of config parser and routing table builder

--- parse.py
+++ parse.py
@@ -3,14 +3,64 @@
 # 20 March 2016
 
 def build_table(config_file):
-    # Opens config file, reads and returns routing table
+    # Opens config file, reads and returns routerid, input ports, routing table
     # @param config_file the filename of the configuration file
 
-    #router id 1 <= x <= 64000, unique
-    #set of port numbers to listen on (neighbours) format eg input-ports 6110, 6201, 7345
-        #port numbers 1024 <= x <= 64000
-    #output ports: portnum-linkmetric-routerid eg. outputs 5000-1-1, 5002-5-4
-        # port numbers 1024 <= x <= 64000, metric 0 >= x >= 16 (Double check this)
-        # no outputs should also be inputs
+    temp = []
+    inputstream = open(config_file, 'r')
+    for line in lines(inputstream):
+        temp.append(line.split(', '))
 
-    raise NotImplementedError
+    #Verify router ID
+    routerid = temp.pop(0)
+    if not ('router-id' in routerid) and (int(routerid[1]) in range(1,64000)):
+        raise Exception
+
+    #Verify input ports
+    input_ports = temp.pop(0)
+    if 'input-ports' in input_ports.pop(0):
+        for port in input_ports:
+            if int(port) not in range(1024,64000):
+                raise Exception
+
+    # Build routing table
+    # Layout of this table is [ID, PORT ,COST,FLAG,TIMER]
+    output_ports = temp.pop(0)
+    routing_table = []
+    flag = False
+    timer = [0,0]
+    for entry in output_ports[1:]:
+        entry = entry.split('-')
+        if verify_output(routerid, input_ports, entry):
+            id = entry[2]
+            port = int(entry[0])
+            cost = int(entry[1])
+            routing_table.append([entry[2],int(entry[0]),int(entry[1]), flag, timer])
+    return routerid[1], input_ports, routing_table
+
+
+def lines(f):
+    # Helper function
+    # Removes blank lines and comments
+
+    for l in f:
+        line = l.rstrip()
+        if (line) and (line[0] != '#'):
+            yield line
+
+
+def verify_output(ownid, input_ports, entry):
+    # ID is unique
+    if entry[2] == ownid[0]:
+        raise Exception
+    # Ports are not input ports and 1024 <= x <= 64000
+    if (entry[0] in input_ports) and (int(entry[0]) not in range(1024,64000)):
+        raise Exception
+    # Cost is valid
+    if int(entry[1]) not in range(0,16):
+        raise Exception
+    return True
+
+
+config = 'config1.txt'
+build_table(config)
