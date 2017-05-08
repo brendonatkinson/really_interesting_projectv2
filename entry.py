b8fa3d3327fbca9fd1f31d58ba23cf1f782840ca
Brendon Atkinson <brendon.atkinson@gmail.com>
2017-05-08 12:07:59 +1200

    Update entry.py

--- entry.py
+++ entry.py
@@ -3,6 +3,10 @@
 # 11 April 2017
 import time
 
+TIMEOUT = 180
+GARBAGE = 120
+RIP_INFINITY = 16
+
 class Entry(object):
 
     def __init__(self, update):
@@ -10,20 +14,20 @@ class Entry(object):
         self.address = update[1]
         self.metric = update[2]
         self.next_hop = update[3]
-        self.timeout = time.time() + 180
-        self.garbage = 120
+        self.timeout = time.time() + TIMEOUT
+        self.garbage = GARBAGE
         self.change_flag = False
         self.expired_flag = False
 
     def reset_timeout(self):
-        self.timeout = time.time() + 180
-        self.garbage = 120
+        self.timeout = time.time() + TIMEOUT
+        self.garbage = GARBAGE
         self.change_flag = False
         self.expired_flag = False
 
     def expired(self):
-        self.metric = 16
-        self.garbage = time.time() + 120
+        self.metric = RIP_INFINITY
+        self.garbage = time.time() + TIMEOUT
         self.change_flag = True
         self.expired_flag = True
 
